656a452d354e5ce4e782f6dd4ac1b2d163b916d4
Steven Nunez <steven.nunez@gmail.com>
2015-09-11 11:32:02 -0400

    Finish Form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 // $(document).ready(function(){
 $(function(){
-  $('.existing_course_name').click(function(){
+  $('.courses').on('click', '.existing_course_name', function(){
     $(this).siblings('.existing_course_description').fadeToggle();
+  })
+
+  $('#course_creation_form').submit(function(e){
+    e.preventDefault();
+    // Read Form data
+    var courseName = $(this).find('#course_name').val();
+    var courseDescription = $(this).find('#course_description').val();
+    // clear form fields
+    $(this).find('#course_name').val("");
+    $(this).find('#course_description').val("");
+    // create Template with Data
+    var template = "<li>" +
+      "<div class='existing_course_name'>" +
+        courseName +
+      "</div>" +
+      "<div class='existing_course_description'>" +
+        courseDescription +
+      "</div>" +
+    "</li>"
+    // Insert into .courses ul
+    $('.courses').append(template);
   })
 })
